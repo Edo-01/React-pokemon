dbec388bd16683fd6bf30a41b8834068c2fb8d74
Edoardo <edoardo.ottolenghi@gmail.com>
2023-07-20 21:29:56 +0200

    realizzata logica filtro

--- src/components/Filtro.jsx
+++ src/components/Filtro.jsx
@@ -3,11 +3,33 @@ import style from "../assets/css/Filtro.module.css";
 import icoApri from "../assets/img/ico-apri.png";
 import icoTendina from "../assets/img/ico-tendina.png";
 
-function Filtro() {
-  const [aperto, setAperto] = useState(false);
+function Filtro({
+  setInputSelect,
+  setOrdineFiltro,
+  ordineFiltro,
+  impostaFiltro,
+  aperto,
+  setAperto,
+  rimuoviFiltri,
+  inputSelect,
+}) {
+  // const [aperto, setAperto] = useState(false);
 
   const divTendaRef = useRef(null);
   const contenutoTendaRef = useRef(null);
+
+  // const [inputSelect, setInputSelect] = useState("normal");
+  // const [ordineFiltro, setOrdineFiltro] = useState("pvasc"); // pvasc-pvdes-numasc-numdes
+  // const [filtro, setFiltro] = useState({
+  //   pokemonType: inputSelect,
+  //   order: ordineFiltro,
+  // });
+  // function impostaFiltro() {
+  //   setFiltro({
+  //     pokemonType: inputSelect,
+  //     order: ordineFiltro,
+  //   });
+  // }
 
   useEffect(() => {
     let misuraEl = contenutoTendaRef.current.getBoundingClientRect().height;
@@ -45,25 +67,33 @@ function Filtro() {
         <div ref={contenutoTendaRef} className={style.gruppoRighe}>
           <div className={style.rigaCorpo + " " + style.rigaUno}>
             <p>Pokèmon type</p>
-            <select name="" id="">
-              <option value="">Normal</option>
-              <option value="">Fire</option>
-              <option value="">Water</option>
-              <option value="">Grass</option>
-              <option value="">Electric</option>
-              <option value="">Ice</option>
-              <option value="">Fighting</option>
-              <option value="">Poison</option>
-              <option value="">Ground</option>
-              <option value="">Flying</option>
-              <option value="">Psychic</option>
-              <option value="">Bug</option>
-              <option value="">Rock</option>
-              <option value="">Ghost</option>
-              <option value="">Dark</option>
-              <option value="">Dragon</option>
-              <option value="">Steel</option>
-              <option value="">Fairy</option>
+            <select
+              onChange={(e) => {
+                setInputSelect(e.target.value);
+              }}
+              name="pokemonType"
+              id=""
+              value={inputSelect}
+            >
+              <option value="normal">Normal</option>
+              <option value="fire">Fire</option>
+              <option value="water">Water</option>
+              <option value="grass">Grass</option>
+              <option value="electric">Electric</option>
+              <option value="ice">Ice</option>
+              <option value="fighting">Fighting</option>
+              <option value="poison">Poison</option>
+              <option value="ground">Ground</option>
+              <option value="flying">Flying</option>
+              <option value="psychic">Psychic</option>
+              <option value="bug">Bug</option>
+              <option value="rock">Rock</option>
+              <option value="ghost">Ghost</option>
+              <option value="dark">Dark</option>
+              <option value="dragon">Dragon</option>
+              <option value="steel">Steel</option>
+              <option value="fairy">Fairy</option>
+              <option value="all">All</option>
             </select>
           </div>
           <div className={style.rigaCorpo + " " + style.rigaDue}>
@@ -72,20 +102,60 @@ function Filtro() {
           <div className={style.rigaCorpo + " " + style.rigaTre}>
             <div className={style.colRiga3A}>
               <p>PV</p>
-              <button className={style.colRiga3BottUno}>Asc</button>
-              <button>Des</button>
+              <button
+                onClick={() => {
+                  setOrdineFiltro("pvasc");
+                }}
+                className={
+                  ordineFiltro === "pvasc"
+                    ? style.colRiga3BottUno + " " + style.attivo
+                    : style.colRiga3BottUno
+                }
+              >
+                Asc
+              </button>
+              <button
+                onClick={() => {
+                  setOrdineFiltro("pvdes");
+                }}
+                className={ordineFiltro === "pvdes" ? style.attivo : null}
+              >
+                Des
+              </button>
             </div>
             <div className={style.colRiga3B}>
               <p>Number</p>
-              <button className={style.colRiga3BottUno}>Asc</button>
-              <button>Des</button>
+              <button
+                onClick={() => {
+                  setOrdineFiltro("numasc");
+                }}
+                className={
+                  ordineFiltro === "numasc"
+                    ? style.colRiga3BottUno + " " + style.attivo
+                    : style.colRiga3BottUno
+                }
+              >
+                Asc
+              </button>
+              <button
+                onClick={() => {
+                  setOrdineFiltro("numdes");
+                }}
+                className={ordineFiltro === "numdes" ? style.attivo : null}
+              >
+                Des
+              </button>
             </div>
           </div>
           <div className={style.rigaCorpo + " " + style.rigaQuattro}>
-            <p>Remove filter</p>
+            <p>
+              <span onClick={rimuoviFiltri}>Remove filter</span>
+            </p>
           </div>
           <div className={style.rigaCorpo + " " + style.rigaCinque}>
-            <button className={style.apply}>Apply</button>
+            <button onClick={impostaFiltro} className={style.apply}>
+              Apply
+            </button>
           </div>
         </div>
       </div>
--- src/components/Preferiti.jsx
+++ src/components/Preferiti.jsx
@@ -4,7 +4,8 @@ import palla from "../assets/img/ico-ball-scheda.png";
 import cestino from "../assets/img/ico-cestino.png";
 import dorme from "../assets/img/pokemon-dorme.png";
 import vedi from "../assets/img/ico-vedi.png";
-import { useEffect, useRef } from "react";
+import pokem from "../assets/img/pokemon-filtro.png";
+import { useEffect, useRef, useState } from "react";
 
 import Filtro from "./Filtro";
 
@@ -13,6 +14,15 @@ function NessunPref() {
     <div className={style.containerNoPref}>
       <img src={dorme} alt="" />
       <h3>You don't have any favourites</h3>
+    </div>
+  );
+}
+
+function FiltroVuoto() {
+  return (
+    <div className={style.containerNoPref}>
+      <img src={pokem} alt="" />
+      <h3>Opss...no match!</h3>
     </div>
   );
 }
@@ -50,6 +60,31 @@ function Preferiti({
   cancellaPref,
   mostraSingolo,
 }) {
+  const [filtroAttivo, setFiltroAttivo] = useState(false); //stato che controllerà la visualizzazione di tutti i preferiti (dato originale) oppure i filtri(copia del originale)
+  const [aperto, setAperto] = useState(false);
+  const [inputSelect, setInputSelect] = useState("normal");
+  const [ordineFiltro, setOrdineFiltro] = useState("pvasc"); // pvasc-pvdes-numasc-numdes
+  const [filtro, setFiltro] = useState({
+    pokemonType: inputSelect,
+    order: ordineFiltro,
+  });
+  function impostaFiltro() {
+    setFiltro({
+      pokemonType: inputSelect,
+      order: ordineFiltro,
+    });
+    setAperto(false);
+    setFiltroAttivo(true);
+  }
+
+  function rimuoviFiltri() {
+    setFiltroAttivo(false);
+    setAperto(false);
+    setInputSelect("normal");
+    setOrdineFiltro("pvasc");
+    setFiltro({ pokemonType: "normal", order: "pvasc" });
+  }
+
   const titleRef = useRef(null);
   useEffect(() => {
     function segna() {
@@ -64,6 +99,75 @@ function Preferiti({
       window.removeEventListener("scroll", segna);
     };
   }, []);
+
+  function ordina(pokemonType, order) {
+    let pokemonFiltartiGlobal = null;
+    if (pokemonType === "all") {
+      pokemonFiltartiGlobal = pokemonPreferiti;
+    } else {
+      const pokemonFiltarti = pokemonPreferiti.filter((obj) => {
+        return obj.types[0].type.name === pokemonType;
+      });
+      const pokemonFiltarti2 = pokemonPreferiti.filter((obj) => {
+        if (obj.types.length === 2) {
+          return obj.types[1].type.name === pokemonType;
+        }
+      });
+      pokemonFiltartiGlobal = pokemonFiltarti.concat(pokemonFiltarti2);
+    }
+
+    console.log(pokemonPreferiti);
+    console.log(pokemonFiltartiGlobal);
+    if (order === "pvasc") {
+      pokemonFiltartiGlobal.sort(function (a, b) {
+        return a.stats[0].base_stat - b.stats[0].base_stat;
+      });
+    } else if (order === "pvdes") {
+      pokemonFiltartiGlobal.sort(function (a, b) {
+        return b.stats[0].base_stat - a.stats[0].base_stat;
+      });
+    } else if (order === "numasc") {
+      pokemonFiltartiGlobal.sort(function (a, b) {
+        return a.order - b.order;
+      });
+    } else if (order === "numdes") {
+      pokemonFiltartiGlobal.sort(function (a, b) {
+        return b.order - a.order;
+      });
+    }
+    return pokemonFiltartiGlobal;
+  }
+
+  const prefertiDatiFiltro = ordina(filtro.pokemonType, filtro.order); // qui ho un array di dati filtrati in base ai parametri
+
+  console.log(prefertiDatiFiltro);
+
+  const soloPreferitiFiltrati = prefertiDatiFiltro.map((obj) => {
+    return (
+      <CardPreferiti
+        key={obj.id}
+        obj={obj}
+        cancellaPref={cancellaPref}
+        cambiaPag={cambiaPag}
+        mostraSingolo={mostraSingolo}
+      />
+    );
+  });
+
+  const tuttiPrefriti = pokemonPreferiti.map((obj) => {
+    return (
+      <CardPreferiti
+        key={obj.id}
+        obj={obj}
+        cancellaPref={cancellaPref}
+        cambiaPag={cambiaPag}
+        mostraSingolo={mostraSingolo}
+      />
+    );
+  });
+
+  ordina(filtro.pokemonType, filtro.order);
+
   return (
     <>
       <div className={style.containerNav}>
@@ -77,28 +181,37 @@ function Preferiti({
       </div>
       {pokemonPreferiti.length === 0 ? null : (
         <div className={style.containerFiltri}>
-          <Filtro />
+          <Filtro
+            setInputSelect={setInputSelect}
+            setOrdineFiltro={setOrdineFiltro}
+            ordineFiltro={ordineFiltro}
+            impostaFiltro={impostaFiltro}
+            aperto={aperto}
+            setAperto={setAperto}
+            rimuoviFiltri={rimuoviFiltri}
+            inputSelect={inputSelect}
+          />
         </div>
       )}
       {pokemonPreferiti.length === 0 ? (
         <NessunPref />
       ) : (
         <div className={style.containerSchede}>
-          {pokemonPreferiti.map((obj) => {
-            return (
-              <CardPreferiti
-                key={obj.id}
-                obj={obj}
-                cancellaPref={cancellaPref}
-                cambiaPag={cambiaPag}
-                mostraSingolo={mostraSingolo}
-              />
-            );
-          })}
+          {filtroAttivo ? (
+            prefertiDatiFiltro.length === 0 ? (
+              <div className={style.containerAvvisoFiltro}>
+                <FiltroVuoto />
+              </div>
+            ) : (
+              soloPreferitiFiltrati
+            )
+          ) : (
+            tuttiPrefriti
+          )}
         </div>
       )}
     </>
   );
 }
-
+// containerAvvisoFiltro
 export default Preferiti;
